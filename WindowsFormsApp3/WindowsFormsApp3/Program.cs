2738e54a98db85b7f991dd4a6925d0bcf31c5c93
d.f.r.woods@gmail.com <d.f.r.woods@gmail.com>
2019-09-25 13:00:57 +0200

    proba

--- WindowsFormsApp3/WindowsFormsApp3/Program.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp3
         [STAThread]
         static void Main()
         {
+            //pls work
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
